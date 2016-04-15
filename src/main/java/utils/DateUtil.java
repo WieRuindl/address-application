6e1b0b42157280fcb44a46fc4c446272829678e5
pwhite <pavelbelov.home@gmail.com>
2016-04-15 19:08:47 +0300

    lesson-3-show-persons

---
+++ src/main/java/utils/DateUtil.java
@@ -0,0 +1,27 @@
+package utils;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+public class DateUtil {
+    private static final String DATE_PATTERN = "dd.MM.yyyy";
+    private static final DateTimeFormatter DATE_FORMATTER =
+            DateTimeFormatter.ofPattern(DATE_PATTERN);
+
+    public static String format(LocalDate date) {
+        return date == null ? null : DATE_FORMATTER.format(date);
+    }
+
+    public static LocalDate parse(String dateString) {
+        try {
+            return DATE_FORMATTER.parse(dateString, LocalDate::from);
+        } catch (DateTimeParseException e) {
+            return null;
+        }
+    }
+
+    public static boolean validate(String dateString) {
+        return DateUtil.parse(dateString) != null;
+    }
+}
--- src/main/java/view/PersonOverviewController.java
+++ src/main/java/view/PersonOverviewController.java
@@ -6,6 +6,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import person.Person;
+import utils.DateUtil;
 
 public class PersonOverviewController {
     @FXML
@@ -32,12 +33,37 @@ public class PersonOverviewController {
 
     @FXML
     private void initialize() {
-        firstNameColumn.setCellValueFactory(cellData -> cellData.getValue().firstNameProperty());
-        lastNameColumn.setCellValueFactory(cellData -> cellData.getValue().lastNameProperty());
+        firstNameColumn.setCellValueFactory(
+                cellData -> cellData.getValue().firstNameProperty());
+        lastNameColumn.setCellValueFactory(
+                cellData -> cellData.getValue().lastNameProperty());
+
+        showPersonDetails(null);
+
+        personTable.getSelectionModel().selectedItemProperty().addListener(
+                (observable, oldValue, newValue) -> showPersonDetails(newValue));
     }
 
     public void setDataContainer(DataContainer dataContainer) {
         this.dataContainer = dataContainer;
         personTable.setItems(dataContainer.getPersonData());
+    }
+
+    private void showPersonDetails(Person person) {
+        if (person != null) {
+            firstNameLabel.setText(person.getFirstName());
+            lastNameLabel.setText(person.getLastName());
+            streetLabel.setText(person.getStreet());
+            postalCodeLabel.setText(Integer.toString(person.getPostalCode()));
+            cityLabel.setText(person.getCity());
+            birthdayLabel.setText(DateUtil.format(person.getBirthday()));
+        } else {
+            firstNameLabel.setText("");
+            lastNameLabel.setText("");
+            streetLabel.setText("");
+            postalCodeLabel.setText("");
+            cityLabel.setText("");
+            birthdayLabel.setText("");
+        }
     }
 }
